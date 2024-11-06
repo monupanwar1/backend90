28a30d59d82905cd53e7d102d33199f3f70dca72
panwarkunal <monupanwar5655@gmail.com>
2024-11-06 19:07:23 +0530

    notifiaction

--- react90/src/App.jsx
+++ react90/src/App.jsx
@@ -3,15 +3,40 @@
 import {useState} from 'react';
 
 
-const App = () => {
+// const App = () => {
 
-    const [count, setCount]=useState(0);
-  return (
-    <>
-    <div>count :{count}</div>
-    <button onClick ={()=>setCount(count+1)}> Increment</button>
-    <button onClick ={()=>setCount(count-1)}> decrement</button>
-    </>
-)
+//     const [count, setCount]=useState(0);
+//   return (
+//     <>
+//     <div>count :{count}</div>
+//     <button onClick ={()=>setCount(count+1)}> Increment</button>
+//     <button onClick ={()=>setCount(count-1)}> decrement</button>
+//     </>
+// )
+// }
+// export default App
+
+
+
+
+const App =()=>{
+
+    let [notification,setNotification] = useState(0);
+
+    const handleClick =()=>{
+        console.log("hihihihih")
+        setNotification(notification+1);
+    }
+
+    return(
+        <div style={{background:"#dfe6ef",height:"100vh"}}>
+            <button onClick={handleClick}>Increment</button>
+            <p>{notification}</p>
+
+
+
+        </div>
+
+    )
 }
-export default App
+export default App;
