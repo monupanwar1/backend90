028c100112c2675ca4d7b3aabf8fbcad4f2370bc
panwarkunal <monupanwar5655@gmail.com>
2024-11-07 20:58:50 +0530

    lists and key

--- react90/src/App.jsx
+++ react90/src/App.jsx
@@ -219,31 +219,65 @@ import {useEffect, useState} from 'react';
 
 
 
-const Collapsible = ({ title, children }) => {
-  const [isOpen, setIsOpen] = useState(false);
+// const Collapsible = ({ title, children }) => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   return (
+//       <div>
+//           <button onClick={() => setIsOpen(!isOpen)}>
+//               {title} {isOpen ? '-' : '+'}
+//           </button>
+//           {isOpen && <div>{children}</div>}
+//       </div>
+//   );
+// };
+
+// const App = () => {
+//   return (
+//       <div>
+//           <Collapsible title="Section 1">
+//               <p>This is the content of section 1.</p>
+//           </Collapsible>
+//           <Collapsible title="Section 2">
+//               <p>This is the content of section 2.</p>
+//           </Collapsible>
+//       </div>
+//   );
+// };
+
+// export default App;
+
+
+//map ,lists and key ;
+
+const Itemlist=({items})=>{
+  return (
+    <ul>
+      {items.map(item=>(
+        <li key={item.id}>
+          {item.name}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+
+const App =()=>{
+
+  const items=[
+    {id:1,name:"item 1"},
+    {id:2,name:"item 2"},
+    {id:3,name:"item 3"},
+    {id:4,name:"item 4"},
+    {id:5,name:"item 5"},
+  ]
 
   return (
-      <div>
-          <button onClick={() => setIsOpen(!isOpen)}>
-              {title} {isOpen ? '-' : '+'}
-          </button>
-          {isOpen && <div>{children}</div>}
-      </div>
-  );
-};
-
-const App = () => {
-  return (
-      <div>
-          <Collapsible title="Section 1">
-              <p>This is the content of section 1.</p>
-          </Collapsible>
-          <Collapsible title="Section 2">
-              <p>This is the content of section 2.</p>
-          </Collapsible>
-      </div>
-  );
-};
+    <div>
+      <Itemlist items={items}/>
+    </div>
+  )
+}
 
 export default App;
-
